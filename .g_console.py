b77a40cd5c2be1f852743fe590c8403567b41844
swalgocoder <gary.d.gao@gmail.com>
2017-02-08 09:31:37 -0800

    Update .g_console.py
    quit at command line still not working yet

--- .g_console.py
+++ .g_console.py
@@ -3,14 +3,29 @@
 
 import cmd
 
-class HelloWorld(cmd.Cmd):
-    """Simple command processor example."""
+class MyConsole(cmd.Cmd):
+
+    prompt = '(hnbb)'
+    """
+    intro = "a custom prompt"
+    """
     
-    def do_greet(self, line):
-        print ("hello")
+    doc_header = 'Documented commands (type help <topic>):'
+    """
+    misc_header = 'misc_header'
+    """
+    """
+    undoc_header = 'undoc_header'
+    """
+    ruler = '='
     
+    def do_quit(self, line):
+        "Quit command to exit the program"
+        self.prompt = line + ': '
+    """
     def do_EOF(self, line):
         return True
+    """
 
 if __name__ == '__main__':
-    HelloWorld().cmdloop()
+    MyConsole().cmdloop()
