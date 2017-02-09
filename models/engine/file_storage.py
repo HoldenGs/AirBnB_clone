871ba7b65275e98d009bf40eac30b89a90099574
Holden Grissett <holdeng96@gmail.com>
2017-02-09 22:15:23 +0000

    add file storage class

---
+++ models/engine/file_storage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+
+class FileStorage:
+    __file_path = storage.json
+    __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects[obj.id] = obj
+
+    def save(self):
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
+
+    def reload(self):
+        with open(self.__file_path, 'r') as file:
+            self.__objects = json.load(file)
