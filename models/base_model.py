851f4c3d399724373c094c1eb889efc40c07715a
Holden Grissett <holdeng96@gmail.com>
2017-02-10 03:47:11 +0000

    fix datetime conversion

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,31 @@
 import datetime
 import uuid
 import copy
+from . import storage
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if len(args) > 0:
+            if type(args[0]) is dict:
+                self.__dict__ = args[0]
+                self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+                self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+                return;
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now())
+        self.created_at = datetime.datetime.now()
+        storage.new(self)
 
     def __str__(self):
-        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = str(datetime.datetime.now())
+        self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_json(self):
+        self.updated_at = datetime.datetime.isoformat(self.updated_at)
+        self.created_at = datetime.datetime.isoformat(self.created_at)
         new_dict = copy.copy(self.__dict__)
-        new_dict['__class__'] = 'BaseModel'
+        new_dict['__class__'] = self.__class__.__name__
         return new_dict
