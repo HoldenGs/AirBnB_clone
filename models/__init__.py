cb015f94fd7054bb15a8142dc0715b0d70b2f61f
Holden Grissett <holdeng96@gmail.com>
2017-02-10 21:20:06 +0000

    change import functionality

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 
-__all__ = ["BaseModel"]
+__all__ = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
 
-from engine import file_storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.engine import file_storage
 
 storage = file_storage.FileStorage()
 storage.reload()
