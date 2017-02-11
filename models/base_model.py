df84d5b6ecc0c187777151647826dc8206ed5303
Holden Grissett <holdeng96@gmail.com>
2017-02-11 01:39:57 +0000

    format to_json time properly

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import datetime
 import uuid
 import copy
-from . import storage
+import models
 
 class BaseModel:
 
@@ -11,23 +11,24 @@ class BaseModel:
         if len(args) > 0:
             if type(args[0]) is dict:
                 self.__dict__ = args[0]
-                self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
-                self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+                self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%d %H:%M:%S.%f")
+                self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%d %H:%M:%S.%f")
                 return;
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        storage.new(self)
+        models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_json(self):
-        self.updated_at = datetime.datetime.isoformat(self.updated_at)
-        self.created_at = datetime.datetime.isoformat(self.created_at)
         new_dict = copy.copy(self.__dict__)
         new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = str(self.updated_at)
+        new_dict['updated_at'] = str(self.created_at)
         return new_dict
