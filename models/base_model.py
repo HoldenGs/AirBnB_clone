1be9f2a37156b06c28d167ba0c85d115734b172b
Holden Grissett <holdeng96@gmail.com>
2017-02-12 03:16:57 +0000

    pep8:

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,25 @@ import uuid
 import copy
 import models
 
+
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if len(args) > 0:
             if type(args[0]) is dict:
                 self.__dict__ = args[0]
-                self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%d %H:%M:%S.%f")
-                self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%d %H:%M:%S.%f")
-                return;
+                self.updated_at = datetime.datetime.strptime(
+                    self.updated_at, "%Y-%m-%d %H:%M:%S.%f")
+                self.created_at = datetime.datetime.strptime(
+                    self.created_at, "%Y-%m-%d %H:%M:%S.%f")
+                return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.datetime.now()
