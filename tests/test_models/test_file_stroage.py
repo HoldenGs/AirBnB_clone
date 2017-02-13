3a2cada4de949cf56929ce59efa3a324130739b6
swalgocoder <gary.d.gao@gmail.com>
2017-02-12 19:02:52 -0800

    Create test_file_stroage.py

---
+++ tests/test_models/test_file_stroage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This unittest tries testing file_storage, incomplete"""
+
+import os
+import tempfile
+import time
+import unittest
+from models import *
+
+
+class FileStorageTest(unittest.TestCase):
+  def setUp(self):
+    self._storage_path = tempfile.mkdtemp()      
+    self._storage = FileStorage.Storage(self._storage_path)
+
+  def tearDown(self):
+    os.removedirs(self._storage_path)
