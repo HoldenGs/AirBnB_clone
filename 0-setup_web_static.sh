f212f004e879783e011755c2b47cf15589665f8b
Holden Grissett <holdeng96@gmail.com>
2017-03-15 23:50:29 +0000

    correct a typo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 # Set up web servers for deployment
+
 if [ ! -d /etc/nginx/ ]; then
     sudo apt-get install -y nginx
 fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "Holberton yo!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+webpath="/data/web_static/releases/test/"
+echo "<html><head></head><body>Holberton Yo!</body></html>" | sudo tee "${webpath}/index.html" > /dev/null
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf "$webpath" /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "29i\        location /hbtn_static/ {\n                alias /data/web_static/current;\n        }\n" /etc/nginx/sites-enabled/default
+sudo grep -q /etc/nginx/sites-enabled/default -e "hbnb_static"
+result=$?
+if [ "$result" -eq 1 ]; then
+    sudo sed -i "29i\        location /hbnb_static/ {\n                alias /data/web_static/current/;\n        }\n" /etc/nginx/sites-available/default
+fi
 sudo service nginx restart
