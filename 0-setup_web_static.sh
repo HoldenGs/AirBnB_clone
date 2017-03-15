62b04308daca28ff7eb130bc2a1fcd492ef88d04
Holden Grissett <holdeng96@gmail.com>
2017-03-15 23:00:27 +0000

    remove directory implication for symlink, restart nginx at end

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,9 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Holberton yo!" > /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
 sudo sed -i "29i\        location /hbtn_static/ {\n                alias /data/web_static/current/;\n" /etc/nginx/sites-enabled/default
+sudo service nginx restart
