c9663dedf01e961a02cdc8b0bbba35c212a389db
Holden Grissett <holdeng96@gmail.com>
2017-03-16 00:45:21 +0000

    pep8 compliance and return file pathname

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ from fabric.api import *
 from time import strftime
 from os import path
 
+
 def do_pack():
     """
     Archive the web_static folder contents in a folder called 'versions'
@@ -12,7 +13,8 @@ def do_pack():
         if not path.exists("./versions/"):
             local("mkdir versions")
         timestr = strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(timestr))
-        return 0
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(timestr))
+        return "versions/web_static_{}.tgz".format(timestr)
     except:
         return None
