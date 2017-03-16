4c5d4b4aa257ff5a9aab94929e8c1ce5e7a8b8de
Holden Grissett <holdeng96@gmail.com>
2017-03-16 00:42:05 +0000

    add file 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from fabric.api import *
+from time import strftime
+from os import path
+
+def do_pack():
+    """
+    Archive the web_static folder contents in a folder called 'versions'
+    """
+    try:
+        if not path.exists("./versions/"):
+            local("mkdir versions")
+        timestr = strftime("%Y%m%d%H%M%S")
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(timestr))
+        return 0
+    except:
+        return None
