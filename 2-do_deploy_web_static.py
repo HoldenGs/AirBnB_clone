3ef28c9faa54acfec5482fea25d645bb4c332896
Holden Grissett <holdeng96@gmail.com>
2017-03-16 03:58:49 +0000

    pep8 compliance"

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,15 +36,15 @@ def do_deploy(archive_path):
         webfolder = archive.split('.')[0]
         run('mkdir -p /data/web_static/releases/{}/'.format(webfolder))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-             .format(archive, webfolder))
+            .format(archive, webfolder))
         run('rm /tmp/{}'.format(archive))
         run('mv /data/web_static/releases/{}/web_static/* \
         /data/web_static/releases/{}/'.format(webfolder, webfolder))
         run('rm -rf /data/web_static/current')
         run('rm -rf /data/web_static/releases/{}/web_static'
-             .format(webfolder))
+            .format(webfolder))
         run('ln -s /data/web_static/releases/{} /data/web_static/current'
-             .format(webfolder))
+            .format(webfolder))
         return True
     except:
         return False
